549e7b0c1366cf6d4b32b0c97d29f75d26e5acdc
Your Name <you@example.com>
2020-04-16 20:20:40 +0500

    upd

--- index.php
+++ index.php
@@ -1,23 +1,78 @@
 <?php
 
-$login = filter_var($_POST['login'] ,FILTER_SANITIZE_STRING);
-$password = filter_var($_POST['password'] ,FILTER_SANITIZE_STRING);;
+session_start();
 
-$users = [
-  ["login" => 'admin@admin', 'password' => '$2y$10$jfyR.6xkg07xGY4/c/YJ4eWMJVmnOjPuBC6CTVhoD.fgSfyE/r2x6'],
-  ["login" => 'guest@guest', 'password' => '$2y$10$kawxkMDPhNmTmo6WhkxV8uzmJRXP8harQvbZAT2e5aChE48yucGl6']
-];
+$requestUri = $_SERVER["REQUEST_URI"];
 
-$currentUser = null;
+if (is_null($_SESSION["currentUser"]) && $requestUri != '/login') {
+    header("Location: /login");
+}
 
-foreach ($users as $user) {
-    if ($user['login'] == $login && password_verify($password, $user['password'])) {
-        $currentUser = $user;
+if ($requestUri == "/logout") {
+    session_destroy();
+    header("Location: /");
+    die();
+}
+
+if ($requestUri == "/login") {
+    include "login.html";
+    die();
+}
+
+if ($requestUri == "/about") {
+    $handleRequest = function() {
+        echo "about";
+    };
+
+    include "layout.php";
+    die();
+}
+
+if ($requestUri == "/profile") {
+    $handleRequest = function() {
+        echo "profile";
+    };
+
+    include "layout.php";
+    die();
+}
+
+if ($requestUri == "/users") {
+    $handleRequest = function() {
+        echo "users";
+    };
+
+    include "layout.php";
+    die();
+}
+
+if ($requestUri == "/") {
+    $handleRequest = function() {
+        echo "ГЛАВНАЯ";
+    };
+
+    include "layout.php";
+    die();
+}
+
+if ($requestUri == "/auth") {
+    $login = filter_var($_POST['login'] ,FILTER_SANITIZE_STRING);
+    $password = filter_var($_POST['password'] ,FILTER_SANITIZE_STRING);
+
+    $users = [
+        ["login" => 'admin@admin', 'password' => '$2y$10$jfyR.6xkg07xGY4/c/YJ4eWMJVmnOjPuBC6CTVhoD.fgSfyE/r2x6'],
+        ["login" => 'guest@guest', 'password' => '$2y$10$kawxkMDPhNmTmo6WhkxV8uzmJRXP8harQvbZAT2e5aChE48yucGl6']
+    ];
+
+    foreach ($users as $user) {
+        if ($user['login'] == $login && password_verify($password, $user['password'])) {
+            $_SESSION["currentUser"] = $user;
+        }
     }
+    header("Location: /");
+    die();
 }
 
-if (is_null($currentUser)) {
-    echo "NE OK";
-} else {
-    echo "OK";
-}
+
+http_response_code(404);
+die();
